87940dd9f98b6608d1eb66264d8d950eb3c670ce
Thomas Jebarsan <142016100+Jeba-Jebarsan@users.noreply.github.com>
2025-02-15 19:31:37 +0530

    fix netlify bug5

--- functions/netlify.edge.ts
+++
@@ -1,5 +0,0 @@
-import { createRequestHandler } from "@remix-run/cloudflare";
-import type { ServerBuild } from "@remix-run/cloudflare";
-import * as build from "../build/server";
-
-export default createRequestHandler(build as unknown as ServerBuild);
