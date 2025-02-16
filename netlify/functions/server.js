ed7125d3443bf6c2599fc4d5aa5a13ac4e114745
Thomas Jebarsan <142016100+Jeba-Jebarsan@users.noreply.github.com>
2025-02-16 11:42:55 +0530

    update after deploy

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -4,4 +4,8 @@ import * as build from "../../build/server/index.js";
 export const handler = createRequestHandler({
   build,
   mode: process.env.NODE_ENV,
+  getLoadContext: (event, context) => ({
+    ...context,
+    env: process.env
+  })
 }); 
